5882a3637d3992230608c048b4924815f95fe18a
emurillo28 <emurillo28@uabc.edu.mx>
2017-11-18 18:20:18 -0800

    Modified tables, now we have actividades, aerobicas, anaerobicas

--- app/src/main/java/com/example/ivan/proyectofinalv2/DatabaseHelper.java
+++ app/src/main/java/com/example/ivan/proyectofinalv2/DatabaseHelper.java
@@ -11,8 +11,9 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "ProjectDB";
-    private static final String CREATE_TB_USER = "create table user(id integer primary key, name text, email text)";
-    private static final String CREATE_TB_RUNNING = "create table running(carrera text, fecha text, hora text, min text, seg text)";
+    private static final String CREAR_TB_ACTIVIDADES = "create table actividades(id integer primary key, Nombre text, Descripcion text, Categoria text)";
+    private static final String CREAR_TB_AEROBICAS = "CREATE  TABLE aerobicas (tiempo FLOAT, distancia FLOAT, descripcion TEXT, fecha DATETIME, id_actividad integer, FOREIGN KEY(id_actividad) REFERENCES  actividades(id))";
+    private static final String CREAR_TB_ANAEROBICAS = "CREATE TABLE anaerobicas(tiempo FLOAT, repeticiones INTEGER,peso FLOAT, descripcion TEXT, fecha DATETIME, id_actividad integer, FOREIGN KEY(id_actividad) REFERENCES  actividades(id))";
 
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DB_NAME, factory, version);
@@ -20,18 +21,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Here we create the database
-        db.execSQL(CREATE_TB_USER);
-        db.execSQL(CREATE_TB_RUNNING);
+        // Creamos Las tablas de la base de datos
+        db.execSQL(CREAR_TB_ACTIVIDADES);
+        db.execSQL(CREAR_TB_AEROBICAS);
+        db.execSQL(CREAR_TB_ANAEROBICAS);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop if exists user");
-        db.execSQL("drop if exists running");
+        db.execSQL("drop if exists actividades");
+        db.execSQL("drop if exists aerobicas");
+        db.execSQL("drop if exists anaerobicas");
 
-        db.execSQL(CREATE_TB_USER);
-        db.execSQL(CREATE_TB_RUNNING);
+        db.execSQL(CREAR_TB_ACTIVIDADES);
+        db.execSQL(CREAR_TB_AEROBICAS);
+        db.execSQL(CREAR_TB_ANAEROBICAS);
     }
 }
