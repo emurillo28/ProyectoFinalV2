e05e0b79bfedb4acc348bc0d3e5f75bf2cecbb45
ivanramirez27 <ivanramirez27@users.noreply.github.com>
2017-12-19 11:50:34 -0800

    Cambios en la opcion de "mas", ya se puede usar

--- app/src/main/java/com/example/ivan/proyectofinalv2/SelectActivity.java
+++ app/src/main/java/com/example/ivan/proyectofinalv2/SelectActivity.java
@@ -5,12 +5,24 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.ivan.proyectofinalv2.entity.Actividad;
+import com.example.ivan.proyectofinalv2.entity.Categoria;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+import static android.R.id.list;
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by IVAN on 01/12/2017.
@@ -18,16 +30,38 @@ import org.w3c.dom.Text;
 
 public class SelectActivity extends AppCompatActivity{
 
-    ListView lstActividades;
+    TextView tvRunning, tvCycling, tvSwimming, tvWalking, tvPesas, tvFuerza;
+    String[] itemname= {
+            "Running", "Ciclismo", "Natacion", "Caminata", "Fuerza", "Pesas"
+    };
+    Integer[] imgid={
+            R.drawable.icons8_running_50,
+            R.drawable.icons8_cycling_50,
+            R.drawable.icons8_swimming_50,
+            R.drawable.icons8_walking_filled_50,
+            R.drawable.icons8_pushups_50,
+            R.drawable.icons8_weightlifting_50,
+    };
+    ListView list;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activities);
-        lstActividades = (ListView) findViewById(R.id.lstActividades);
-    }
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, imgid);
+        list=(ListView)findViewById(R.id.lst_Actividades);
+        list.setAdapter(adapter);
 
-    public void launchEnterActivity(View v) {
-        Intent EnterActivity = new Intent(getApplicationContext(), EnterActivityData.class);
-        startActivity(EnterActivity);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                String Slecteditem = itemname[+position];
+                Log.d(TAG,Slecteditem);
+                Intent EnterActivity = new Intent(getApplicationContext(), EnterActivityData.class);
+                EnterActivity.putExtra("activity", Slecteditem);
+                startActivity(EnterActivity);
+            }
+        });
     }
 }
